cd794bbe651fd5c1e3e190eed384f4b116c6f4b2
Calupoh <marco@calupoh.ml>
2023-01-23 19:01:19 -0600

    comando basicos

---
+++ README.md
@@ -0,0 +1,3 @@
+# Mi paguina personal
+
+Estas iendo el coigo de mi sitio web personal (aun en construcción). Esta escrioto con Flask y Gunicorn para poser aplicar un proxi inverso
--- src/my_site_calupoh/__init__.py
+++ src/my_site_calupoh/__init__.py
@@ -3,7 +3,7 @@ import os
 from flask import Flask
 
 
-def create_app(test_config=None):
+def create_app(test_config=None, invers_prox=False):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
@@ -17,6 +17,14 @@ def create_app(test_config=None):
     else:
         # load the test config if passed in
         app.config.from_mapping(test_config)
+    
+    # configurando para funcionar detras de un proxi
+    if invers_prox:
+        from werkzeug.middleware.proxy_fix import ProxyFix
+
+        app.wsgi_app = ProxyFix(
+            app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
+        )
 
     # ensure the instance folder exists
     try:
